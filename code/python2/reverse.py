2eb2190924cf1c75bde91ff915c9939683dd3e3c
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2011-02-14 16:50:20 +0000

    Updating reverse.py

--- code/python2/reverse.py
+++ code/python2/reverse.py
@@ -1,3 +1,13 @@
+"""
+
+Code example for Think Python
+http://thinkpython.com
+
+Copyright 2008 Allen B. Downey.
+Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
+
+"""
+
 def in_both(word1, word2):
     for letter in word1:
         if letter in word2:
@@ -21,7 +31,22 @@ def is_reverse(word1, word2):
 
     return True
 
+def all_but_first(word):
+    return word[1:]
+
+def all_but_last(word):
+    return word[:-1]
+
+def is_reverse(word1, word2):
+    print word1, word2
+    if word1 == '' or word2 == '':
+        return word1 == '' and word2 == ''
+
+    if word1[0] != word2[-1]:
+        return False
+
+    return is_reverse(word1[1:], word2[:-1])
 
 print is_reverse('pots', 'stop')
 print is_reverse('spot', 'pots')
-print is_reverse('pot', 'stop')
+print is_reverse('ab', 'ba')

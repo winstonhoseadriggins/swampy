32e5d995816fa8d0cfa6894731d1e1642b0cf9aa
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2011-03-15 16:37:02 +0000

    Updating remote_object

--- code/python2/remote_object.py
+++ code/python2/remote_object.py
@@ -133,13 +133,13 @@ class NameServer:
         """Unregister all objects in the given group that start
         with the given prefix."""
         t = self.ns.list(group)
-        print t
+
         for (s, n) in t:
             if not s.startswith(prefix): continue
             if n==1:
                 if group:
                     s = '%s.%s' % (group, s)
-                print s
+                print 'Unregistering', s
 	        self.ns.unregister(s)
     
 
@@ -201,6 +201,7 @@ class RemoteObject(Pyro.core.ObjBase):
             self.pyro_daemon.disconnect(self)
         except:
             print "Did not disconnect cleanly."
+            raise sys.exc_type, sys.exc_value
         self.stopLoop()
         self.pyro_daemon.shutdown()
 
@@ -228,54 +229,18 @@ class RemoteObject(Pyro.core.ObjBase):
             self.thread.join()
 
 
-def main(script, name='remote_object', group='test', *args):
+def main(script, action='', prefix='', group=None):
 
-    # find the name server
-    ns = NameServer()
-
-    # if the group already exists, delete it
-    if ns.query(group) == 0:
-        print 'Deleting group %s...' % group
-        ns.delete_group(group)
-
-    # make the group
-    print 'Creating group %s...' % group
-    ns.create_group(group)
-
-    # create a remote object and connect it
-    full_name = '%s.%s' % (group, name)
-    server = RemoteObject(full_name, ns)
-
-    # confirm that the group and object are on the name server
-    print group, ns.query(group)
-    print full_name, ns.query(name, group)    
-    print group, ns.get_remote_object_list(group=group)
-
-    # create a Watcher and then run the server loop in a thread
-    watcher = Watcher(server.cleanup)
-
-    # here's what the child thread runs
-    def client_code(full_name, server):
-        # get a proxy for this object
-        # and invoke a method on it
+    if action == 'clear':
         ns = NameServer()
-        proxy = ns.get_proxy(full_name)
-        
-        # invoke one of its methods remotely
-        print proxy.__hash__()
+        if prefix == '':
+            prefix = 'remote_object'
+        ns.clear(prefix, group)
 
-        # stop the server
-        server.stopLoop()
-        server.join()
-
-        # child thread completes
-        print 'Thread complete.'
-
-    child = MyThread(client_code, full_name, server)
-
-    # here's what the parent thread runs
-    server.stoppableLoop()
-    print 'Server done.'
+    if action == 'list':
+        ns = NameServer()
+        t = ns.get_remote_object_list(prefix, group)
+        print t
 
 
 if __name__ == '__main__':
--- code/python2/remote_object_test.py
+++ code/python2/remote_object_test.py
@@ -1,72 +1,64 @@
-"""This module is part of Swampy, a suite of programs available from
-allendowney.com/swampy.
+"""Test code for remote_object.py
 
 Copyright 2010 Allen B. Downey
 Distributed under the GNU General Public License at gnu.org/licenses/gpl.html.
 """
 
-import unittest
+import sys
 
 import remote_object
 
-class Tests(unittest.TestCase):
+def main(script, name='remote_object', group='test', *args):
 
-    def test_remote_object(self):
-        name = 'remote_object'
-        group = 'test'
+    # find the name server
+    ns = remote_object.NameServer()
 
-        # find the name server
-        ns = remote_object.NameServer()
-
-        # if it doesn't have a group named test, make one
-        if ns.query(group) == 0:
-            ns.delete_group(group)
-            
-        print 'Making group %s...' % group
-        ns.create_group(group)
-
-        # create a remote object and connect it
-        full_name = '%s.%s' % (group, name)
-        server = remote_object.RemoteObject(full_name, ns)
-
-        # confirm that the group and object are on the name server
-        self.assertEquals(ns.query(group), 0)
-        self.assertEquals(ns.query(name, group), 1)   
-        
-        t = ns.get_remote_object_list(group=group)
-        self.assertTrue(name in t)
-
-        # create a Watcher and then run the server loop in a thread
-        try:
-            watcher = remote_object.Watcher(server.cleanup)
-        except SystemExit:
-            print 'Watcher exited.'
-
-        child = remote_object.MyThread(client_code, full_name, server)
-        server.stoppableLoop()
-        print 'Server done.'
-
+    # if the group already exists, delete it
+    if ns.query(group) == 0:
+        print 'Deleting group %s...' % group
         ns.delete_group(group)
 
-        # confirm that the group and object are gone
-        self.assertEquals(ns.query(group), -1)
+    # make the group
+    print 'Creating group %s...' % group
+    ns.create_group(group)
 
+    # create a remote object and connect it
+    full_name = '%s.%s' % (group, name)
+    server = remote_object.RemoteObject(full_name, ns)
 
-def client_code(full_name, server):
-    # get a proxy for this object
-    # and invoke a method on it
-    ns = remote_object.NameServer()
-    proxy = ns.get_proxy(full_name)
-    print proxy
+    # confirm that the group and object are on the name server
+    print group, ns.query(group)
+    print full_name, ns.query(name, group)    
+    print group, ns.get_remote_object_list(group=group)
 
-    # stop the server
-    server.stopLoop()
-    server.join()
+    # create a Watcher and then run the server loop in a thread
+    watcher = remote_object.Watcher(server.cleanup)
 
-    # child thread completes
-    print 'Thread complete.'
+    # here's what the child thread runs
+    def client_code(full_name, server):
+        # get a proxy for this object
+        # and invoke a method on it
+        ns = remote_object.NameServer()
+        proxy = ns.get_proxy(full_name)
+        
+        # invoke one of its methods remotely
+        print proxy.__hash__()
 
+        # stop the server
+        server.stopLoop()
+        server.join()
 
+        # child thread completes
+        print 'Thread complete.'
+
+    child = remote_object.MyThread(client_code, full_name, server)
+
+    # here's what the parent thread runs
+    server.stoppableLoop()
+    print 'Server done.'
+
+    print 'Deleting group %s...' % group
+    ns.delete_group(group)
 
 if __name__ == '__main__':
-    unittest.main()
+    main(*sys.argv)

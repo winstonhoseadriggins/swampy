5c15dc7e96c19cd658d005e0e096eb585773591c
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2011-03-15 19:42:22 +0000

    Adding KangarooWorld

---
+++ code/python2/KangarooWorld.py
@@ -0,0 +1,33 @@
+from Gui import Gui
+
+import Lumpy
+lumpy = Lumpy.Lumpy()
+lumpy.make_reference()
+
+class World(Gui):
+    def __init__(self):
+        Gui.__init__(self)      # invoke the init method from the parent
+        self.animals = []
+
+    def register(self, animal):
+        self.animals.append(animal)
+
+class Animal(object):
+    def __init__(self, world):
+        self.world = world
+        world.register(self)
+
+class Kangaroo(Animal):
+    def __init__(self, world):
+        Animal.__init__(self, world)   # invoke the init method from the parent
+        self.pouch = []
+
+world = World()
+
+kanga = Kangaroo(world)
+roo = Kangaroo(world)
+
+kanga.pouch.append(roo)
+
+lumpy.object_diagram()
+lumpy.class_diagram()

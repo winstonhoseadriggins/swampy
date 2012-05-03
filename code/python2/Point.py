c399d002090979c27e04c0a09e68ef006f4f79ef
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2012-05-03 13:28:12 +0000

    Updating Time2, adding Point

--- code/python2/Point.py
+++ code/python2/Point.py
@@ -1,2 +1,41 @@
+#!/usr/bin/python
+
 class Point:
-    """represents a point in 2-D space"""
+  def __init__(self, x=0, y=0):
+    self.x = x
+    self.y = y
+
+  def __str__(self):
+    return '(' + str(self.x) + ', ' + str(self.y) + ')'
+
+  def __add__(self, other):
+    return Point(self.x + other.x, self.y + other.y)
+
+  def __sub__(self, other):
+    return Point(self.x - other.x, self.y - other.y)
+
+  def __mul__(self, other):
+    return self.x * other.x + self.y * other.y
+
+  def __rmul__(self, other):
+    return Point(other * self.x, other * self.y)
+
+  def reverse(self):
+    self.x, self.y = self.y, self.x
+
+  def frontAndBack(front):
+    from copy import copy
+    back = copy(front)
+    back.reverse()
+    print str(front) + str(back)
+
+p = Point (1, 2)
+print p
+p.reverse()
+print p
+
+def print_attributes(obj):
+    for attr, val in obj.__dict__.items():
+        print attr, getattr(obj, attr)
+
+print_attributes(p)

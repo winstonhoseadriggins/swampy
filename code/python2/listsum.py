00738e79c361ce7d064c1ee1b814bc9e5205857d
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2013-11-18 15:40:49 +0000

    Updating listsum.py

--- code/python2/listsum.py
+++ code/python2/listsum.py
@@ -2,7 +2,7 @@
 Think Python by Allen B. Downey
 http://thinkpython.com
 
-Copyright 2012 Allen B. Downey
+Copyright 2013 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 
 """
@@ -14,13 +14,20 @@ import matplotlib.pyplot as pyplot
 def etime():
     """Measures user and system time this process has used.
 
-    Returns the sum of user and system time."""
+    returns: sum of user and system time.
+    """
     user, sys, chuser, chsys, real = os.times()
     return user+sys
 
 
-def sum_plus(t, init):
-    """Concatenates a list of lists into a single list using +="""
+def sum_pluseq(t, init):
+    """Concatenates a list of lists into a single list using +=
+    
+    t: list of lists
+    init: initialization for the sum
+
+    returns: list
+    """
     total = init
     for x in t:
         total += x
@@ -28,7 +35,13 @@ def sum_plus(t, init):
 
 
 def sum_extend(t, init):
-    """Concatenates a list of lists into a single list using list.extend"""
+    """Concatenates a list of lists into a single list using list.extend
+
+    t: list of lists
+    init: initialization for the sum
+
+    returns: list
+    """
     total = init
     for x in t:
         total.extend(x)
@@ -36,13 +49,24 @@ def sum_extend(t, init):
 
 
 def sum_sum(t, init):
-    """Concatenates a list of lists into a single list using sum"""
+    """Concatenates a list of lists into a single list using sum.
+    
+    t: list of lists
+    init: initialization for the sum
+
+    returns: list
+    """
     return sum(t, init)
 
 
 def test_func(f, n):
-    """Tests the function (f) with a list of lists of length (n)
-    and return the elapsed time."""
+    """Tests the function, f, with a list of lists of length n.
+
+    f: function that flattens a list of lists
+    n: input size
+
+    returns: elapsed time in seconds
+    """
     t = [[1]] * n
 
     start = etime()
@@ -55,7 +79,7 @@ def test_func(f, n):
 def test(name):
     """Tests the given function with a range of values for n.
 
-    Returns a list of ns and a list of run times.
+    Returns: list of ns and a list of run times.
     """
     # look up the string (name) and get the function object
     f = eval(name)
@@ -63,7 +87,7 @@ def test(name):
     # depending on which function we are testing, we need to
     # use a different order of magnitude for (n)
     d = dict(sum_sum=1000,
-             sum_plus=100000,
+             sum_pluseq=100000,
              sum_extend=100000)
     factor = d[name]
 
@@ -90,8 +114,10 @@ def plot(ns, ts, label, color='blue', exp=1.0):
     exp: exponent (slope) for the fitted curve
     """
     tfit = fit(ns, ts, exp)
-    pyplot.plot(ns, tfit, color='0.7', linewidth=2, linestyle='dashed')
-    pyplot.plot(ns, ts, label=label, color=color, linewidth=3)
+    pyplot.plot(ns, tfit, color='0.7',
+                linewidth=2, alpha=0.5,linestyle='dashed')
+    pyplot.plot(ns, ts, label=label, color=color,
+                linewidth=3, alpha=0.5)
 
 
 def fit(ns, ts, exp=1.0, index=-1):
@@ -99,6 +125,10 @@ def fit(ns, ts, exp=1.0, index=-1):
 
     Use the given index as a reference point, and scale all other
     points accordingly.
+
+    ts: sequence of t (run time)
+    exp: exponent (slope) for the fitted curve
+    index: which point to use as a reference point
     """
     nref = ns[index]
     tref = ts[index]
@@ -124,7 +154,14 @@ def save(root, exts=['eps', 'pdf']):
         pyplot.savefig(filename)
 
 
-def make_fig(funcs, scale='log', exp=1.0, filename=''):
+def make_fig(funcs, scale='log', exp=1.0, root=''):
+    """Makes a figure with the given funcs.
+
+    funcs: list of function names
+    scale: string scale for the plot: 'log' or 'linear'
+    exp: expected exponent of the data
+    root: string filename root (without extension)
+    """
     pyplot.clf()
     pyplot.xscale(scale)
     pyplot.yscale(scale)
@@ -139,15 +176,15 @@ def make_fig(funcs, scale='log', exp=1.0, filename=''):
 
     pyplot.legend(loc=4)
 
-    if filename:
-        save(filename)
+    if root:
+        save(root)
     else:
         pyplot.show()
 
 
 def main(script):
-    make_fig(['sum_extend', 'sum_plus'], exp=1.0, filename='listsum1')
-    make_fig(['sum_sum'], exp=2.0, filename='listsum2')
+    make_fig(['sum_extend', 'sum_pluseq'], exp=1.0, root='listsum1')
+    make_fig(['sum_sum'], exp=2.0, root='listsum2')
 
 
 if __name__ == '__main__':

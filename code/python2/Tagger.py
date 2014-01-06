90113a3b680923d8651cb8fb11e5572d5f9927e8
allendowney <allendowney@0997dd22-e62c-0410-aaa9-01c0db2e315f>
2014-01-06 19:08:57 +0000

    Updating Tagger.py

--- code/python2/Tagger.py
+++ code/python2/Tagger.py
@@ -20,7 +20,12 @@ class Tagger(Wobbler):
     """Represents a Turtle that plays tag."""
 
     def __init__(self, world, speed=1, clumsiness=60, color='red'):
+
+        # call the __init__ method from Wobbler to initialize
+        # world, speed, clumsiness, and color
         Wobbler.__init__(self, world, speed, clumsiness, color)
+
+        # then initialize the additional attributes that Taggers have
         self.it = 0
         self.sulking = 0
 
